0084bc539fc745c96231fc6209f887f7b964bd25
Wasif Shah <wasif1024@gmail.com>
2023-02-05 23:16:29 +0500

    latest

---
+++ cmd/net/ping.go
@@ -0,0 +1,69 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package net
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"time"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	urlPath string
+	client  = http.Client{
+		Transport: &http.Transport{Dial: (&net.Dialer{Timeout: 2 + time.Second}).Dial},
+	}
+)
+
+func ping(domain string) (int, error) {
+	url := "http://" + domain
+	req, err := http.NewRequest("HEAD", url, nil)
+	if err != nil {
+		return 0, err
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return 0, err
+	}
+	resp.Body.Close()
+	return resp.StatusCode, nil
+
+}
+
+// pingCmd represents the ping command
+var PingCmd = &cobra.Command{
+	Use:   "ping",
+	Short: "This pings a remote url",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		//fmt.Println("ping called")
+		if resp, err := ping(urlPath); err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(resp)
+		}
+	},
+}
+
+func init() {
+	PingCmd.Flags().StringVarP(&urlPath, "url", "u", "", "the url to ping")
+
+	if err := PingCmd.MarkFlagRequired("url"); err != nil {
+		fmt.Println(err)
+	}
+	//rootCmd.AddCommand(pingCmd)
+	NetCmd.AddCommand(PingCmd)
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// pingCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// pingCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
